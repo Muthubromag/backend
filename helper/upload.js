442d8fa989ddd238fb0add1bc8ca0979e730e4e1
Mahendrasinghmanikandan <mahendrasinghmani222@gmail.com>
2023-12-15 19:04:19 +0530

    path changed

--- helper/upload.js
+++ helper/upload.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, path.join("./public/images"));
+    return cb(null, path.join(__dirname, "../public", "Images"));
   },
   filename: function (req, file, cb) {
     return cb(null, `${Date.now()}_${file.originalname}`);
